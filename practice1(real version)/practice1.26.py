a5241eba55f514644536dcd16a27cd2ddf15404a
01egedary <imanzhantigr07@gmail.com>
2026-01-31 02:16:26 +0500

    My homework

---
+++ practice1(real version)/practice1.26.py
@@ -0,0 +1,29 @@
+
+text = "It's a \"Python\" string\nNew line here"
+print(text)
+
+
+
+word = "Programming"
+print(word[0:6])   
+print(word[-3:])  
+
+
+
+message = "  hello python  "
+print(message.upper())     
+print(message.strip())     
+print(message.replace("python", "world"))
+
+
+
+first_name = "Anna"
+last_name = "Smith"
+full_name = first_name + " " + last_name
+print(full_name)
+
+
+
+age = 21
+result = f"My name is {full_name} and I am {age} years old."
+print(result)
